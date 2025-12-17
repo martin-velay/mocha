daca78703ec71f28fb81c405553104e2acfb8c4c
Douglas Reis <doreis@lowrisc.org>
2025-12-17 16:44:36 +0000

    [sw] Remove the temporary software build scripts

--- sw/device/examples/hello_world/build.sh
+++
@@ -1,6 +0,0 @@
-# Copyright lowRISC contributors (COSMIC project).
-# Licensed under the Apache License, Version 2.0, see LICENSE for details.
-# SPDX-License-Identifier: Apache-2.0
-riscv64-unknown-elf-gcc -march=rv64imc -mabi=lp64 -static -mcmodel=medany -Wall -g -fvisibility=hidden -ffreestanding -c hello_world.c
-riscv64-unknown-elf-gcc -march=rv64imc -mabi=lp64 -static -mcmodel=medany -Wall -g -fvisibility=hidden -ffreestanding -c boot.S
-riscv64-unknown-elf-ld -nostdlib -Tlink.ld hello_world.o boot.o -o hello_world.elf
